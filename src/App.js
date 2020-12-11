8e4fd409ef60e80420bdbf9e5da2384ed18e6211
Reid Relatores <rrrelatores@gmail.com>
2020-12-10 21:11:50 -0800

    added some freaking maps man

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from "react"
 import NavBar from "./Components/NavBar"
 import ZipCodeInput from "./Components/ZipCodeInput"
+import Map from "./Components/Map"
 
 class onePercent extends React.Component {
   render() {
@@ -9,7 +10,9 @@ class onePercent extends React.Component {
         <div id="main">
             <NavBar></NavBar>
             <div class="top">
+                <p>SUP</p>
                 <h2>onePercent</h2>
+                <p>Welcome to Reid's first webpage yo</p>
                 <div class="zipinput">
                     <ZipCodeInput></ZipCodeInput>
                 </div>
@@ -18,7 +21,9 @@ class onePercent extends React.Component {
                 <div id="middle" class="urls">
                     <div id="urls" class="links"></div>
                 </div>
-                <div id="middle" class="map"></div>
+                <div id="middle" class="map">
+                    <Map/>
+                </div>
                 <div id="middle" class="cases">
                     <div id="cases" class="best-case"></div>
                     <div id="cases" className="worst-case"></div>
